99733bd57931ac1d78b1bb83e413706b68047e32
aherschel <ademp90@gmail.com>
2024-06-10 14:15:34 -0700

    flip to rust-tools, remove rustaceanvim

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -17,15 +17,6 @@ local plugins = {
     end,
   },
   {
-    "mrcjkb/rustaceanvim",
-    version = "^4",
-    ft = { "rust" },
-    dependencies = "neovim/nvim-lspconfig",
-    config = function()
-      require "custom.configs.rustaceanvim"
-    end
-  },
-  {
     "mfussenegger/nvim-dap",
     init = function()
       require("core.utils").load_mappings("dap")
@@ -70,6 +61,17 @@ local plugins = {
       table.insert(M.sources, {name = "crates"})
       return M
     end,
-  }
+  },
+  {
+    "simrat39/rust-tools.nvim",
+    ft = "rust",
+    dependencies = "neovim/nvim-lspconfig",
+    opts = function ()
+      return require "custom.configs.rust-tools"
+    end,
+    config = function(_, opts)
+      require("rust-tools").setup(opts)
+    end
+  },
 }
 return plugins
