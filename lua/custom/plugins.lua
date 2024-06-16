dced8353ccf69b974bfad7b006418ae987fd3e04
aherschel <ademp90@gmail.com>
2024-06-15 18:26:30 -0700

    add golang support

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -83,5 +83,32 @@ local plugins = {
       require('gh-actions').setup(opts)
     end,
   },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    ft = "go",
+    opts = function()
+      return require "custom.configs.null-ls"
+    end,
+  },
+  {
+    "dreamsofcode-io/nvim-dap-go",
+    ft = "go",
+    dependencies = "mfussenegger/nvim-dap",
+    config = function(_, opts)
+      require("dap-go").setup(opts)
+      require("core.utils").load_mappings("dap_go")
+    end
+  },
+  {
+    "olexsmir/gopher.nvim",
+    ft = "go",
+    config = function(_, opts)
+      require("gopher").setup(opts)
+      require("core.utils").load_mappings("gopher")
+    end,
+    build = function()
+      vim.cmd [[silent! GoInstallDeps]]
+    end,
+  },
 }
 return plugins
